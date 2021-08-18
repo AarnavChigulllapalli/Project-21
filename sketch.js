6a992e66259b1fab1f8a551d2058b5bacb5412c8
AarnavChigulllapalli <86109544+AarnavChigulllapalli@users.noreply.github.com>
2021-08-18 14:51:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
 var Bathing, Brushing, Eating, Gym, Moving, Astronaut, Sleep, bg
 
-function Preload(){
-  Bathing.loadAnimation("bath1.png","bath2.png")
-  Brushing.loadAnimation("brush.png")
-  Eating.loadAnimation("eat1.png","eat2.png")
-  Gym.loadAnimation("gym1.png","gym2.png","gym11.png","gym12.png")
-  Moving.loadAnimation("move.png","move1.png")
-  Sleep.loadAnimation("sleep.png")
-  bg.loadImage("iss.png")
+function preload(){
+  Bathing=loadAnimation("images/bath1.png","images/bath2.png")
+  Brushing=loadImage("images/brush.png")
+  Eating=loadAnimation("images/eat1.png","images/eat2.png")
+  Gym=loadAnimation("images/gym1.png","images/gym2.png","images/gym11.png","images/gym12.png")
+  Moving=loadAnimation("images/move.png","images/move1.png")
+  Sleep=loadImage("images/sleep.png")
+  bg=loadImage("images/iss.png")
 }
 
 function setup() {
   createCanvas(400, 400);
   Astronaut.createSprite(300,230);
-  Astronaut.addAnimation(Sleep)
+  Astronaut.addImage(Sleep)
   Astronaut.scale = 0.1
 }
 
@@ -25,39 +25,39 @@ function draw() {
   text("Left arrow to eating",200,250)
   text("M to moving",200,300)
   if(keyDown("UP_ARROW")){
-    Astronaut.addAnimation(Brushing)
-    Astronaut.changeAnimation("Brushing", Brushing)
+    Astronaut.addImage(Brushing)
+    Astronaut.changeImage("Brushing", Brushing)
     Astronaut.y=350
-    Astronaut.valocityX=0
-    Astronaut.valocityY=0
+    Astronaut.velocityX=0
+    Astronaut.velocityY=0
   } 
   if(keyDown("DOWN_ARROW")){
     Astronaut.addAnimation(Gym)
     Astronaut.changeAnimation("Gym", Gym)
     Astronaut.y=350
-    Astronaut.valocityX=0
-    Astronaut.valocityY=0
+    Astronaut.velocityX=0
+    Astronaut.velocityY=0
   } 
   if(keyDown("RIGHT_ARROW")){
     Astronaut.addAnimation(Bathing)
     Astronaut.changeAnimation("Bathing", Bathing)
     Astronaut.y=350
-    Astronaut.valocityX=0
-    Astronaut.valocityY=0
+    Astronaut.velocityX=0
+    Astronaut.velocityY=0
   } 
   if(keyDown("LEFT_ARROW")){
     Astronaut.addAnimation(Eating)
     Astronaut.changeAnimation("Eating", Eating)
     Astronaut.y=350
-    Astronaut.valocityX=0
-    Astronaut.valocityY=0
+    Astronaut.velocityX=0
+    Astronaut.velocityY=0
   } 
   if(keyDown("M")){
     Astronaut.addAnimation(Moving)
     Astronaut.changeAnimation("Moving", Moving)
     Astronaut.y=350
-    Astronaut.valocityX=0.1
-    Astronaut.valocityY=0.1
+    Astronaut.velocityX=0.1
+    Astronaut.velocityY=0.1
   } 
-  drawSprits()
+  drawSprites()
 }
